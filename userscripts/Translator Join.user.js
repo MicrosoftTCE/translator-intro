f1491d9cc18b0d51bea7805699185bfdaea6b643
Ross Dakin <rossdakin@gmail.com>
2016-10-06 00:26:17 -0400

    Add userscript to prepopulate name, language
    Name: Applicant (HH:MM ZZ)
    Language: (specify code as ?lang=enUS URL param)

---
+++ userscripts/Translator Join.user.js
@@ -0,0 +1,79 @@
+// ==UserScript==
+// @name         Translator Join
+// @namespace    http://microsoftnewyork.com
+// @version      0.1
+// @description  Augment functionality of Translator's "join" interface.
+// @author       Ross Dakin
+// @match        https://msrmtc01.azurewebsites.net/join*
+// @grant        none
+
+// ==/UserScript==
+
+(function() {
+  'use strict';
+  
+  // can we dry this up by accessing the params above somehow?
+  console.debug('[IDNYC Translator Join] loaded version 0.1');
+  
+  function getUrlParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+  }
+  
+  function setScopeParam(scope, paramName, value) {
+    console.debug('[IDNYC Translator Join] old', paramName, scope[paramName]);
+    console.debug('[IDNYC Translator Join] new', paramName, value);
+    
+    scope[paramName] = value;
+  }
+  
+  function setLanguageCode(scope) {
+    var newLanguageCode = getUrlParameterByName('lang');
+    
+    if (!newLanguageCode) {
+      console.warn('[IDNYC Translator Join] new languageCode in falsy; aborting');
+      return;
+    }
+    
+    setScopeParam(scope, 'languageCode', newLanguageCode);
+  }
+  
+  function setApplicantName(scope) {
+    var date = new Date();
+    var hour = (date.getHours() - (date.getHours() >= 12 ? 12 : 0)) || 12;
+    var period = date.getHours() >= 12 ? 'PM' : 'AM';
+    var time = hour + ':' + date.getMinutes() + ' ' + period;
+    var name = 'Applicant (' + time + ')';
+    
+    setScopeParam(scope, 'nickname', name);
+  }
+  
+  function getScope(callback) {
+    var scope = angular.element('#languageCode').scope();
+    console.debug('[IDNYC Translator Join] scope:', scope ? scope : 'falsy');
+
+    if (scope) {
+      callback(scope);
+    } else {
+      window.setTimeout(function() {
+        getScope(callback);
+      }, 10); // takes about 40ms in practice
+    }
+  }
+
+  function init() {
+    getScope(function(scope) {
+      setLanguageCode(scope);
+      setApplicantName(scope);
+      
+      scope.$apply();
+    });
+  }
+  
+  init();
+})();
